45dae4c9b0cd82620dc5086c3b81834ae0a2fe03
Acer <goskovd2017@mail.ru>
2022-03-05 03:12:48 +0300

    Add Maven Class

--- src/main/java/ru/sfedu/javaProject/Main.java
+++ src/main/java/ru/sfedu/javaProject/Main.java
@@ -2,6 +2,6 @@ package ru.sfedu.javaProject;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        System.out.println("Hello World");
     }
 }
