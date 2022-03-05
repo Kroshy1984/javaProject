50adebe0b14a07930a0f023f246adc895308bd00
Mikhail Eremeev <meremeev@sfedu.ru>
2022-03-05 16:45:44 +0300

    build: add application configuration

---
+++ src/main/java/ru/sfedu/javaProject/Main.java
@@ -0,0 +1,8 @@
+package ru.sfedu.javaProject;
+
+public class Main {
+    public static void main(String[] args) {
+        final String greeting = "Hello, World!";
+        System.out.println(greeting);
+    }
+}
