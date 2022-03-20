69a63127d651ecb939d92ceb2c9deba1f66cd2c5
eugeny <ya.kocaba@yandex.ua>
2022-03-20 11:11:55 +0300

    1.1 Спринг

--- src/main/java/ru/sfedu/javaProject/Main.java
+++ src/main/java/ru/sfedu/javaProject/Main.java
@@ -1,5 +1,7 @@
 package ru.sfedu.javaProject;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import ru.sfedu.javaProject.api.DataProvider;
 import ru.sfedu.javaProject.api.DataProviderJDBC;
 import ru.sfedu.javaProject.model.User;
@@ -7,9 +9,11 @@ import ru.sfedu.javaProject.model.User;
 import java.io.IOException;
 import java.sql.SQLException;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        User user = new User();
+        SpringApplication.run(Main.class, args);
+        /*User user = new User();
         user.setFirstname("Дмитрий");
         user.setSurname("Харатян");
         user.setPatronymic("Александрович");
@@ -24,7 +28,8 @@ public class Main {
             DataProviderJDBC.getInstance().dropTables();
         } catch (SQLException | IOException | ClassNotFoundException throwables) {
             throwables.printStackTrace();
-        }
+        }*/
+
     }
 
 }
