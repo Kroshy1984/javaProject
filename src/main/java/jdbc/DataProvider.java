c37be9417a0fdd31d3ac95e7e641d76897df0a90
SyberiaImperor <Al.B.danilenko@gmail.com>
2022-03-26 14:40:12 +0300

    user data class and data provider interface added

---
+++ src/main/java/jdbc/DataProvider.java
@@ -0,0 +1,18 @@
+package jdbc;
+
+import jdbc.Model.User;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Optional;
+
+public interface DataProvider {
+
+    void createUser(User user) throws SQLException, IOException;
+
+    Optional<User> getUserById(Long id) throws SQLException, IOException;
+
+    void updateUser(User user) throws SQLException, IOException;
+
+    void deleteUserById(Long id) throws SQLException, IOException;
+}
