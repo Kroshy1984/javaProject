89d87d6906512ec8d574fc47a3867b71d8c31f4a
Mikhail Eremeev <meremeev@sfedu.ru>
2022-04-01 21:22:10 +0300

    feat: add spring boot application

--- src/main/java/ru/sfedu/javaProject/Main.java
+++
@@ -1,8 +0,0 @@
-package ru.sfedu.javaProject;
-
-public class Main {
-    public static void main(String[] args) {
-        final String greeting = "Hello, World!";
-        System.out.println(greeting);
-    }
-}
