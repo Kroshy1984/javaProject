06c2172794a0b205bd80aca1a905f0a251a9ccea
Acer <goskovd2017@mail.ru>
2022-04-09 13:42:53 +0300

    updated default service and pair

--- src/main/java/ru/sfedu/javaProject/model/Pair.java
+++ src/main/java/ru/sfedu/javaProject/model/Pair.java
@@ -14,9 +14,12 @@ public class Pair {
     @Column(nullable = false)
     private Long id;
 
-    @Column(name = "first_user_id", nullable = false)
-    private Long firstUserId;
+    @ManyToMany(cascade = { CascadeType.ALL })
+    @JoinTable(
+            name = "Pairs_Users",
+            joinColumns = { @JoinColumn(name = "pair_id") },
+            inverseJoinColumns = { @JoinColumn(name = "user_id") }
+    )
 
-    @Column(name = "second_user_id", nullable = false)
-    private Long secondUserId;
+    private Set<User> users = new HashSet<>();
 }
--- src/main/java/ru/sfedu/javaProject/services/DefaultService.java
+++ src/main/java/ru/sfedu/javaProject/services/DefaultService.java
@@ -15,13 +15,30 @@ public class DefaultService {
     @Autowired
     DefaultService(UserRepository userRepository) {
         this.userRepository = userRepository;
+        this.pairRepository = pairRepository;
     }
 
     public Optional<SUser> getUserById(Long id) {
         return userRepository.findUserById(id);
     }
 
+    public Iterable<User> getAllUsers() { return userRepository.findAll(); }
+
     public void createUser(SUser user){
         userRepository.save(user);
+    }
+
+    public void createPair(Pair pair) throws {
+        List<User> users = new ArrayList<>(pair.getUsers());
+        if(!pairRepository.findAllByUsersContiansAndUsersContains(users.get(0),users.get(1)).iterator().hasNext())
+            pairRepository.save(pair);
+    }
+
+    public Optional<Pair> getPairById(Long id){
+        return pairRepository.findById(id);
+    }
+
+    public Iterable<Pair> getAllPairs(){
+        
     }
 }
