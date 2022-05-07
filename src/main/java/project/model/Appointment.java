de8e0dbd52bde86f8fae864f7e44e3ce92a7065f
zadro <zadroshy@gmail.com>
2022-05-07 13:01:52 +0300

    edlenko

--- src/main/java/project/model/Appointment.java
+++ src/main/java/project/model/Appointment.java
@@ -1,4 +1,25 @@
 package project.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long appointmentId;
+
+    public long pairId;
+
+    public Date date;
+
+    public boolean success;
 }
--- src/main/java/project/model/Pair.java
+++ src/main/java/project/model/Pair.java
@@ -4,10 +4,24 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
 
 @Entity
 @Data
 @NoArgsConstructor
 public class Pair {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    public long fUserId;
+
+    public long sUserId;
+
+    public Date date;
+
 
 }
