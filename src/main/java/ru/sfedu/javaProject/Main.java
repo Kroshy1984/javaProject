a126062518c252c516d67f6c3ff093d0929d75c0
Dytish <73854732+Dytish@users.noreply.github.com>
2022-06-04 11:18:06 +0300

    Add files via upload
    new

--- src/main/java/ru/sfedu/javaProject/Main.java
+++ src/main/java/ru/sfedu/javaProject/Main.java
@@ -14,9 +14,9 @@ public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
         /*User user = new User();
-        user.setFirstname("Дмитрий");
-        user.setSurname("Харатян");
-        user.setPatronymic("Александрович");
+        user.setFirstname("Даша");
+        user.setSurname("Михник");
+        user.setPatronymic("Романовна");
 
         try {
             DataProvider dataProvider = DataProviderJDBC.getInstance();
